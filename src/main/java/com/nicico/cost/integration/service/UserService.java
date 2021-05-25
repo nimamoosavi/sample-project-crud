90ee3530dd332195ec24eaa2a58f7cf47546140b
nima <nmoosavi@live.com>
2021-05-25 14:40:01 +0430

    bug fix

--- src/main/java/com/nicico/cost/integration/service/UserService.java
+++ src/main/java/com/nicico/cost/integration/service/UserService.java
@@ -2,6 +2,9 @@ package com.nicico.cost.integration.service;
 
 
 import com.nicico.cost.crud.service.GeneralService;
+import com.nicico.cost.framework.anotations.Log;
+import com.nicico.cost.framework.domain.dto.BaseDTO;
+import com.nicico.cost.framework.packages.audit.view.AuditFactory;
 import com.nicico.cost.integration.domain.entity.User;
 import com.nicico.cost.integration.domain.view.request.UserReqVM;
 import com.nicico.cost.integration.domain.view.response.UserResVM;
@@ -9,4 +12,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends GeneralService<User, UserReqVM, UserResVM, Long> {
+    @Override
+    @Log(type = AuditFactory.AuditType.ALL)
+    public BaseDTO<UserResVM> save(UserReqVM userReqVM) {
+        return super.save(userReqVM);
+    }
 }
