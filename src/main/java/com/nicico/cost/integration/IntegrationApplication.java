01cee19ee96a71e107bb581864f83e0b958f5b92
nima <nmoosavi@live.com>
2021-06-12 15:19:28 +0430

    clean code

--- src/main/java/com/nicico/cost/integration/IntegrationApplication.java
+++ src/main/java/com/nicico/cost/integration/IntegrationApplication.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.nicico.cost.crud","com.nicico.cost.audit","com.nicico.cost.swagger", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient", "com.nicico.cost.integration"})
-@ServletComponentScan(basePackages = {"com.nicico.cost.crud","com.nicico.cost.swagger", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient"})
-@EnableJpaRepositories(basePackages = {"com.nicico.cost.crud","com.nicico.cost.swagger", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient", "com.nicico.cost.integration"})
+@ComponentScan({"com.nicico.cost.crud","com.nicico.cost.audit", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient", "com.nicico.cost.integration"})
+@ServletComponentScan(basePackages = {"com.nicico.cost.crud", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient"})
+@EnableJpaRepositories(basePackages = {"com.nicico.cost.crud", "com.nicico.cost.framework", "com.nicico.cost.jdbcclient", "com.nicico.cost.integration"})
 public class IntegrationApplication {
 
     public static void main(String[] args) {
