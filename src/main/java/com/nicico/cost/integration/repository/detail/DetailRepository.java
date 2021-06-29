720bb0977ace63b787dd95267630e3de5a9dc337
nima <nmoosavi@live.com>
2021-06-29 12:12:53 +0430

    change structure for tree

--- src/main/java/com/nicico/cost/integration/repository/detail/DetailRepository.java
+++ src/main/java/com/nicico/cost/integration/repository/detail/DetailRepository.java
@@ -1,18 +1,9 @@
 package com.nicico.cost.integration.repository.detail;
 
 import com.nicico.cost.integration.domain.entity.Detail;
-import com.nicico.cost.integration.domain.entity.DetailType;
 import com.nicico.cost.tree.repository.impl.jdbc.TreeJdbcRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 interface DetailRepository extends TreeJdbcRepository<Detail, Long> {
-
-    List<Detail> findAllByDetailTypesAndIsFolderFalse(DetailType detailType);
-
-    Page<Detail> findAllByDetailTypesAndIsFolderFalse(DetailType detailType, Pageable pageable);
 }
