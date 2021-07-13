85335efd13ba6c58e59764a3622762bda26cec91
nima <nmoosavi@live.com>
2021-07-13 12:48:24 +0430

    add cost and account to integration

--- src/main/java/com/nicico/cost/integration/domain/view/detailtype/DetailTypeReqVM.java
+++ src/main/java/com/nicico/cost/integration/domain/view/detailtype/DetailTypeReqVM.java
@@ -1,4 +1,7 @@
 package com.nicico.cost.integration.domain.view.detailtype;
 
+import lombok.Data;
+
+@Data
 public class DetailTypeReqVM {
 }
--- src/main/java/com/nicico/cost/integration/domain/view/people/PeopleReqVM.java
+++ src/main/java/com/nicico/cost/integration/domain/view/people/PeopleReqVM.java
@@ -1,4 +1,7 @@
 package com.nicico.cost.integration.domain.view.people;
 
+import lombok.Data;
+
+@Data
 public class PeopleReqVM {
 }
